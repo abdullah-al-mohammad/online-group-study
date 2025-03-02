cfea46baf61525326042ad0c0b1c03c803441f84
abdullah-al-mohammad <abdullahalmuhammad6@gmail.com>
2025-03-02 22:02:50 +0600

    add delted data if user and data creator same person

--- src/pages/Assignments/AssignmentCard.jsx
+++ src/pages/Assignments/AssignmentCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import { ImBin } from "react-icons/im";
 import { RxUpdate } from "react-icons/rx";
 import { MdOutlinePreview } from "react-icons/md";
@@ -10,15 +10,38 @@ const AssignmentCard = ({ assignment }) => {
   const { title, image, marks, difficulty, _id } = assignment;
   const { user } = useAuth();
   const axiosPublic = useAxiosPublic();
+  console.log(assignment.email);
 
   const handleDelete = async () => {
     console.log("user deleted");
-    axiosPublic.delete(`/assignment/${assignment._id}`)
-    .then(res => {
-      if(res.data.deletedCount > 0){
-        alert('deleted successfully')
+    if (user?.email !== assignment.email) {
+      return Swal.fire({
+        icon: "error",
+        title: "oops",
+        text: "You can't delete this assignment"
+      })
+    }
+
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!"
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        const res = await axiosPublic.delete(`/assignment/${assignment._id}`)
+        if (res.data.deletedCount > 0) {
+          Swal.fire({
+            title: "Deleted!",
+            text: "Your file has been deleted.",
+            icon: "success"
+          });
+        }
       }
-    })
+    });
   };
   return (
     <div className="card bg-base-100 w-96 h-96 shadow-xl">
@@ -35,7 +58,7 @@ const AssignmentCard = ({ assignment }) => {
         </p>
         <div className="card-actions justify-end">
           <button
-            onClick={handleDelete}
+            onClick={() => handleDelete()}
             className="btn btn-error tooltip"
             data-tip="Delete"
           >
